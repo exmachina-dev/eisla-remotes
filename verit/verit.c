e503fd2a60e3f202063d488bc92798d0ce1ce7fa
Benoit <benoit.rapidel@exmachina.fr>
2015-03-09 16:43:24 +0100

    Reorganized eisla-remotes
    Moved eisla and swill files
    Added verit files

---
+++ verit/verit.c
@@ -0,0 +1,146 @@
+#include <Arduino.h>
+#include <eisla.h>
+#include <verit.h>
+
+volatile bool _EncoderPinASet;
+volatile bool _EncoderPinBSet;
+
+volatile long _EncoderTicks = 0;
+long lastEncoderTicks = 0;
+
+static unsigned int statusLedState = LOW;
+static unsigned long statusLedTime = 0;
+
+static unsigned long speedResolveTime = 0;
+float speed;
+
+
+int deviceId;
+
+eislaCmd inputCmd;
+bool newCmdEvent = false;
+
+void setup() {
+    // declare status led as output
+    pinMode(StatusLed, OUTPUT);
+    digitalWrite(StatusLed, HIGH);
+
+    // Encoder pin A
+    pinMode(EncoderPinA, INPUT);      // sets pin A as input
+    digitalWrite(EncoderPinA, LOW);   // turn on pullup resistors
+    attachInterrupt(EncoderPinAInterrupt, handleEncoderPinA, RISING);
+
+    // Encoder pin B
+    pinMode(EncoderPinB, INPUT);      // sets pin A as input
+    digitalWrite(EncoderPinB, LOW);   // turn on pullup resistors
+    attachInterrupt(EncoderPinBInterrupt, handleEncoderPinB, RISING);
+
+    // Serial port
+    Serial.begin(SwillSerialBaud);
+
+    get_device_infos(&verit);
+    if(verit.deviceId == 0)
+    {
+        set_device_id(&verit, 1);
+        save_device_infos(&verit);
+    }
+
+    digitalWrite(StatusLed, LOW);
+}
+
+void loop() {
+    if((micros()-speedResolveTime) >= (long)0)
+    {
+        long deltaTime = micros() - speedResolveTime;
+        long deltaTicks = lastEncoderTicks - _EncoderTicks;
+        speed = deltaTime / deltaTicks;
+
+        lastEncoderTicks = _EncoderTicks;
+        speedResolveTime = micros() + SpeedResolveDelay;
+    }
+
+
+    if(newCmdEvent)
+    {
+        switch(inputCmd.command) {
+            eislaCmd _cmd;
+            case 'R':
+                if(0 <= inputCmd.data.toInt.int0 < 255)
+                {
+                    set_device_id(&verit, inputCmd.data.toInt.int0);
+                    save_device_infos(&verit);
+                }
+                else
+                {
+                    send_device_infos(&verit);
+                }
+                break;
+            case 'T':
+                send_ticks();
+                break;
+            case 'S':
+                send_speed();
+                break;
+        }
+
+        newCmdEvent = false;
+    }
+
+    if((long)(millis()-statusLedTime) >= 0)
+    {
+        statusLedState =  (statusLedState==HIGH ? LOW : HIGH);
+        digitalWriteFast(StatusLed, statusLedState);
+        statusLedTime = millis()+StatusBlinkDelay;
+    }
+}
+
+void serialEvent()
+{
+    if(Serial.available() > 0)
+    {
+        inputCmd.command = Serial.read();
+        #ifdef EISLA_DEBUG
+        if(Serial.readBytes((char*)inputCmd.data.toBytes, 4))
+        {
+            Serial.print("CMD: ");
+            Serial.print(inputCmd.command);
+            Serial.print(" DAT: ");
+            Serial.println((char*)inputCmd.data.toBytes);
+        }
+        #else
+        Serial.readBytes((char*)inputCmd.data.toBytes, 4);
+        #endif // EISLA_DEBUG
+        newCmdEvent = true;
+    }
+}
+
+void send_ticks()
+{
+    eislaCmd _c;
+    _c.command = 'T';
+    _c.data.toLong = _EncoderTicks;
+    digitalWrite(StatusLed, HIGH);
+    send(_c);
+}
+
+void send_speed()
+{
+    eislaCmd _c;
+    _c.command = 'S';
+    _c.data.toFloat = speed;
+    send(_c);
+}
+
+void handleEncoderPinA()
+{
+    _EncoderPinBSet = digitalReadFast(EncoderPinB);
+
+    _EncoderTicks -= _EncoderPinBSet ? -1 : +1;
+}
+
+void handleEncoderPinB()
+{
+    _EncoderPinASet = digitalReadFast(EncoderPinA);
+
+    _EncoderTicks -= _EncoderPinASet ? -1 : +1;
+}
---
+++ verit/verit.h
@@ -0,0 +1,19 @@
+#ifndef VERIT_H_INCLUDED
+#define VERIT_H_INCLUDED
+#include <Arduino.h>
+
+#define StatusLed 13
+#define StatusBlinkDelay 400        // in milliseconds
+
+#define SpeedResolveDelay 10000     // in microseconds ( 10000µs = 10ms )
+#define EncoderPinAInterrupt 0
+#define EncoderPinBInterrupt 1
+#define EncoderPinA 2
+#define EncoderPinB 3
+
+#define TicksPerRevolution 4096
+
+
+eislaDevice verit = {VERIT};
+
+#endif
