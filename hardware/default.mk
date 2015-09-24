607ac842e58781e36f9f6a48d9834ea14d008592
Benoit Rapidel <benoit.rapidel+devs@exmachina.fr>
2015-09-24 18:26:35 +0200

    Use external resonator; stop overriding fuse from boards.txt

--- hardware/default.mk
+++ hardware/default.mk
@@ -2,10 +2,10 @@ BOARDS_TXT = ../hardware/boards.txt
 BOARD_TAG = eislaremotes
 BOARD_SUB = 8MHzatmega328
 
-# Overriding default fuse
-ISP_LOW_FUSE = 0x62
-ISP_HIGH_FUSE = 0xD8
-ISP_EXT_FUSE = 0x07
+# # Overriding default fuse # Only for debugging purposes
+# ISP_LOW_FUSE = 0xFF
+# ISP_HIGH_FUSE = 0xDA
+# ISP_EXT_FUSE = 0x05
 
 USER_LIB_PATH = ../lib
 ARDUINO_LIBS = eisla EEPROM
