6faadaee6e33947bac5545c631455572108f2f68
Benoit Rapidel <benoit.rapidel+devs@exmachina.fr>
2016-06-03 18:22:05 +0200

    Removed useless files
    such as .o .d files and metada dir

--- varmo_lite/Debug/Project_Settings/Startup_Code/startup.d
+++
@@ -1,8 +0,0 @@
-Project_Settings/Startup_Code/startup.o: \
- ../Project_Settings/Startup_Code/startup.c \
- C:/Users/Adrian/Documents/repos/eisla-remotes/varmo_lite/Generated_Code/IO_Map.h \
- C:/Users/Adrian/Documents/repos/eisla-remotes/varmo_lite/Static_Code/IO_Map/MK20D7.h
-
-C:/Users/Adrian/Documents/repos/eisla-remotes/varmo_lite/Generated_Code/IO_Map.h:
-
-C:/Users/Adrian/Documents/repos/eisla-remotes/varmo_lite/Static_Code/IO_Map/MK20D7.h:
