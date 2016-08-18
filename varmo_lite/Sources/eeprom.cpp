15d9fcfcb94702cb61be0f85f6c2fd0c88c6e4d6
Badoil Adrian <adrian.badoil@exmachina.fr>
2016-08-18 16:16:06 +0200

    Create eeprom file

---
+++ varmo_lite/Sources/eeprom.cpp
@@ -0,0 +1,15 @@
+/*
+ * eeprom.cpp
+ *
+ *  Created on: 2016-08-18
+ *      Author: Adrian
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif
---
+++ varmo_lite/Sources/eeprom.h
@@ -0,0 +1,22 @@
+/*
+ * eeprom.h
+ *
+ *  Created on: 2016-08-18
+ *      Author: Adrian
+ */
+
+#ifndef SOURCES_EEPROM_H_
+#define SOURCES_EEPROM_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif
+
+
+
+#endif /* SOURCES_EEPROM_H_ */
