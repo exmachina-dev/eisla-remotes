4e7b6e9fac6a0ad6af0ea359c129ec9b00f80ff3
Badoil Adrian <adrian.badoil@exmachina.fr>
2016-09-07 19:31:38 +0200

    Fixbug  feedback on instant velocity mode

--- varmo_lite/Sources/protocol.cpp
+++ varmo_lite/Sources/protocol.cpp
@@ -189,20 +189,20 @@ bool msg_processing(int n, ...){
  			return 0;
  		}
  		else if (strcmp(data, Velocity_ref) == 0){
- 			data = va_arg(arg, char*);
- 			velocity_ref_processing(data);
- 			if (menu_indicator == Position_vel_selected){
- 				FLAG_SHORT_CUT = 1;
+ 			if (menu_indicator != Velocity_instant_selected){
+				data = va_arg(arg, char*);
+				velocity_ref_processing(data);
+				if (menu_indicator == Position_vel_selected){
+					FLAG_SHORT_CUT = 1;
+				}
+				FLAG_UPDATE_VALUE = 1;
  			}
- 			FLAG_UPDATE_VALUE = 1;
  			return 0;
  		}
  		else if (strcmp(data, Position_ref) == 0){
- 			if (menu_indicator != Velocity_instant_selected){
-				data = va_arg(arg, char*);
-				position_ref_processing(data);
-				FLAG_UPDATE_VALUE = 1;
- 			}
+			data = va_arg(arg, char*);
+			position_ref_processing(data);
+			FLAG_UPDATE_VALUE = 1;
  			return 0;
  		}
  		else if (strcmp(data, Torque_ref) == 0){
